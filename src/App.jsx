60f417f6c722ce4e1d0d0f4cea34fa65d903c244
‘RSpidla’ <rspidla@gmail.com>
2019-03-12 04:16:51 +0000

    Connected to websocket server

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,8 @@ class App extends Component {
           id: '2'
         }
       ],
-      currentUser: {name: 'Raymond'}
+      currentUser: {name: 'Raymond'},
+      webSocket: new WebSocket('ws://0.0.0.0:3001')
     }
     this.sendMessage = this.sendMessage.bind(this);
   }
@@ -46,16 +47,26 @@ class App extends Component {
   }
 
   componentDidMount() {
-    console.log("componentDidMount <App />");
-    setTimeout(() => {
-    console.log("Simulating incoming message");
-    // Add a new message to the list of messages in the data store
-    const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
-    const messages = this.state.messages.concat(newMessage)
-    // Update the state of the app component.
-    // Calling setState will trigger a call to render() in App and all child components.
-    this.setState({messages: messages})
-    }, 3000);
+    // console.log("componentDidMount <App />");
+    
+    this.state.webSocket.onopen = (event) => {
+      console.log('Client connected to websocket');
+    }
+    
+    
+    
+    
+    // setTimeout(() => {
+    // console.log("Connected to Server");
+    
+    
+    // // Add a new message to the list of messages in the data store
+    // const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+    // const messages = this.state.messages.concat(newMessage)
+    // // Update the state of the app component.
+    // // Calling setState will trigger a call to render() in App and all child components.
+    // this.setState({messages: messages})
+    // }, 3000);
   }
 
   render() {

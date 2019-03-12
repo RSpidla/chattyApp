58fe4f7b55f3ec5592556d021432d9b57c859f7e
‘RSpidla’ <rspidla@gmail.com>
2019-03-12 01:17:25 +0000

    Boilerplate Set-up

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import React, {Component} from 'react';
+import Message from './Message.jsx';
+import ChatBar from './ChatBar.jsx';
+import MessageList from './MessageList.jsx';
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      // <h1>Hello React :)</h1>
+
+      <div>
+        <ChatBar></ChatBar>,
+        <Message>
+          <MessageList></MessageList>
+        </Message>
+      </div>
+
+      
+      // <Message></Message>
+
+
     );
   }
 }
